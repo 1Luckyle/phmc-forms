13785d5840b06c0e4800ea3f4197068aab43355d
1Luckyle <clement.barnouin@gmail.com>
2026-01-09 10:30:27 +0100

    Changement des URLs

--- src/components/FormImageLink.js
+++ src/components/FormImageLink.js
@@ -25,28 +25,28 @@ const FormImageLink = ({
 
     switch (bbCodeVersion) {
         case 1:
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=267";
+            linkHref = "";
             imgSrc = deathReportImage;
             imgAlt = "Lien vers les rapports de décès";
             linkClass = deathReportClass;
             shouldRender = true;
             break;
         case 4:
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=266";
+            linkHref = "";
             imgSrc = deathReportImage;
             imgAlt = "Lien vers les rapports de décès";
             linkClass = deathReportClass;
             shouldRender = true;
             break;
                     case 8:
-            linkHref = "https://phmc.gta.world/viewforum.php?f=266";
+            linkHref = "";
             imgSrc = deathReportImage;
             imgAlt = "Lien vers les rapports de décès";
             linkClass = deathReportClass;
             shouldRender = true;
             break;
         case 11:
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=267";
+            linkHref = "";
             imgSrc = deathReportImage;
             imgAlt = "Lien vers les rapports de décès";
             linkClass = deathReportClass;
@@ -54,7 +54,7 @@ const FormImageLink = ({
             break;
 
         case 24:
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=109";
+            linkHref = "";
             imgSrc = civilianPaperworkImage;
             imgAlt = "Demande de dossiers médicaux";
             linkClass = civilianPaperworkClass;
@@ -62,27 +62,27 @@ const FormImageLink = ({
             break;
         case 3:
         case 25: // This handles both bbCodeVersion 3 and 25
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=221";
+            linkHref = "";
             imgSrc = civilianPaperworkImage;
             imgAlt = "Dossier patient de base";
             linkClass = civilianPaperworkClass;
             shouldRender = true;
             break;
         case 26: // This handles both bbCodeVersion 3 and 25
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=221";
+            linkHref = "";
             imgSrc = civilianPaperworkImage;
             imgAlt = "Mise à jour des dossiers médicaux";
             linkClass = civilianPaperworkClass;
             shouldRender = true;
             break;
         case 35:
-            linkHref = "https://phmc.gta.world/ucp.php?i=pm&mode=compose";
+            linkHref = "";
             imgSrc = civilianPaperworkImage;
             imgAlt = "Demande de certificat médical";
             shouldRender = true;
             break;
         case 37: 
-            linkHref = "https://phmc.gta.world/posting.php?mode=post&f=404";
+            linkHref = "";
             imgSrc = deathReportImage;
             imgAlt = "Lien vers les rapports de décès";
             linkClass = deathReportImage;
@@ -92,7 +92,7 @@ const FormImageLink = ({
         default:
             // Handle the complex PHMC condition that's not a simple bbCodeVersion match
             if (selectedAgencyGroup === 'PHMC' && ![1, 2, 3, 4, 24, 25, 26].includes(bbCodeVersion)) {
-                linkHref = "https://phmc.gta.world/viewforum.php?f=97";
+                linkHref = "";
                 imgSrc = civilianPaperworkImage;
                 imgAlt = "Zone du personnel - Dossiers médicaux";
                 linkClass = civilianPaperworkClass;
